2582cc4f31589ed66bca8a104bd4bedf2b889543
cat-JS-0610-xyz <72459371+cat-JS-0610-xyz@users.noreply.github.com>
2021-04-30 12:14:54 +0700

    Update coder.py

--- coder.py
+++ coder.py
@@ -1,8 +1,11 @@
 from tkinter import *
 encrypt_for_copy = " "
 crypt = 1
-original = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "u", "r", "s", "t", "u", "v", "w", "x", "y", "z",'1','2','3','4','5','6','7','8','9','0','!','?','"',',','.','(',')',' ']
-code =     ["×","Ø","Þ","Ԡ","Ӝ","Հ","Ԕ","Ԫ","ݚ","¶","ݿ","ᴂ","⁜","₮","₡","₯","₱","₪","₻","₧","‰","₷","₫","₰","ℳ","►","ꝑ","Ꝣ","ꝟ","ꭗ","ﮰ","₨","₳", "§", '¿', 'å','ɷ', 'ʥ','ʭ','ʧ','Ѯ', 'Ә', 'Ԭ','֍','♪','אַ','ﷺ','ﻼ','⑫','Ꝋ','Ꜯ','Ꞧ','ﬄ','Ꝿ','ꭚ','ꭐ','ꝃ','ꞗ','Ꝇ','˧','ͳ','Ω','Σ','ϔ','Ͼ','Ѩ','҈','ӂ','փ','☏','✄','↝','❉','❮','δ','ℬ','⑤']
+choise = 1
+language = 0
+original = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",'1','2','3','4','5','6','7','8','9','0','!','?','"',',','.','(',')',' ']
+code =     ["×","Ø","Þ","Ԡ","Ӝ","Հ","Ԕ","Ԫ","ݚ","¶","ݿ","ᴂ","⁜","₮","₡","₯","₱","₪","₻","₧","‰","₷","₫","₰","ℳ","►","ꝑ","Ꝣ","ꝟ","ꭗ","ﮰ","₨","₳", "§", '¿', 'å','ɷ', 'ʥ','ʭ','ʧ','Ѯ', 'Ә', 'Ԭ','֍','♪','אַ','ﷺ','ﻼ','⑫','Ꝋ','Ꜯ','Ꞧ','☏','Ꝿ','ꭚ','ꭐ','ꝃ','ꞗ','Ꝇ','˧','ͳ','Ω','Σ','ϔ','Ͼ','⑤','҈','ӂ','փ','ﬄ','✄','↝','❉','❮','δ','ℬ','c']
+code2=[]
 def show_all(): 
     global original, code
     table = Toplevel() 
@@ -15,7 +18,7 @@ def show_all():
     text.delete(1.0,"end")
     for step in range(0,len(original)-1):
         textInput = textInput + original[step] + " = " + code[step] + "\n"
-    textInput = textInput + "space = ⑤" + "\n"
+    textInput = textInput + "space = Ѩ" + "\n"
     text.insert(1.0,textInput)
     scroll = Scrollbar(table, command=text.yview)
     scroll.pack(side=LEFT, fill=Y)
@@ -52,11 +55,52 @@ def dee():
     crypt = 2
     b1.config(text="decrypt")
     l2.config(text="Decrypted code")
+def choice_1():
+    choise = 1
+def choice_2():
+    choise = 2
+def choice_3():
+    choise = 3
+def rus():
+    language = 1
+    l1.config(text="Напишите текст")
+    print(language)
+    if crypt == 1:
+        b1.config(text="зашифровать")
+    elif crypt == 2:
+        b1.config(text="расшифровать")
+def eng():
+    language = 0
+    l1.config(text="Write a text")
+    print(language)
+    if crypt == 1:
+        b1.config(text="encrypt")
+    elif crypt == 2:
+        b1.config(text="decrypt")
 root = Tk()
 var = IntVar()
+war = IntVar()
 var.set(0)
-root.geometry('400x275+200+100')
+war.set(0)
+root.geometry('400x300+200+100')
 root.title("coder")
+mainmenu = Menu(root)
+root.config(menu=mainmenu)
+mainmenu.config(backgroun="black")
+mainmenu.add_command(label='File')
+#mainmenu.add_command(label='encryption')
+
+choicemenu = Menu(mainmenu, tearoff=0)
+langmenu = Menu(mainmenu, tearoff=0)
+choicemenu.add_command(label="encription one", command = choice_1)
+choicemenu.add_command(label="encription two", command = choice_2)
+choicemenu.add_command(label="encription trhee", command = choice_3)
+choicemenu.add_separator()
+choicemenu.add_command(label="add encription...")
+langmenu.add_command(label="english", command = eng)
+langmenu.add_command(label="русский", command = rus)
+mainmenu.add_cascade(label="encription", menu=choicemenu)
+mainmenu.add_cascade(label="language", menu=langmenu)
 root.resizable(False, False)
 l1 = Label(text="Write a text")
 l1.config(font=("Verdana", 15, 'bold'))
@@ -87,6 +131,6 @@ rb2.place(x=150,y=150)
 l2.place(x=112,y=168)
 l3.place(x=98,y=200)
 b3.place(x=40,y=232)
-#l4.place(x=120,y=265)
 #l5.place(x=47,y=300)
 root.mainloop()
+
